442303edc5d43e9b591afd630984b1d9ceda8c9a
Sulaiman Oboody <lsafer@cufy.org>
2023-01-10 20:19:43 +0300

    build: sources publishing fix 1

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,6 +53,11 @@ tasks.withType<KotlinCompile> {
 }
 
 afterEvaluate {
+    java {
+        withSourcesJar()
+        withJavadocJar()
+    }
+
     publishing {
         publications {
             create<MavenPublication>("maven") {
@@ -65,6 +70,11 @@ afterEvaluate {
 
 subprojects {
     group = "org.cufy.graphkt"
+
+    java {
+        withSourcesJar()
+        withJavadocJar()
+    }
 
     afterEvaluate {
         publishing {
