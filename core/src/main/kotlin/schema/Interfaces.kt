291931216eb7370477bde547151e3fc334d7b3e7
Sulaiman Oboody <lsafer@cufy.org>
2023-05-14 04:57:26 +0300

    chore(core): JVM name of Interfaces.kt

--- core/src/main/kotlin/schema/Interfaces.kt
+++ core/src/main/kotlin/schema/Interfaces.kt
@@ -13,8 +13,6 @@
  *	See the License for the specific language governing permissions and
  *	limitations under the License.
  */
-@file:JvmName("Definitions")
-
 package org.cufy.graphkt.schema
 
 import kotlin.reflect.KMutableProperty1
